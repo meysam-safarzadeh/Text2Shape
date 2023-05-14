533d070fb6498e47796e9ddd485e636167085f77
Meysam Safarzadeh <m.s.97.eng@gmail.com>
2023-05-14 00:31:29 -0400

    tested text encoder and text preprocessing

--- text2shape/models/encoders.py
+++ text2shape/models/encoders.py
@@ -7,72 +7,46 @@ Created on Sat May 13 16:42:31 2023
 """
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from encoder_utils import extract_last_output, compute_sequence_length
 
-class CNNRNNTextEncoder(nn.Module):
-    """CNN-RNN Text Encoder network.
-    """
+class Text_Encoder(nn.Module):
+    def __init__(self):
+        super(Text_Encoder, self).__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv1d(in_channels=768, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv1d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU()
+        )
+        self.gru = nn.GRU(input_size=256, hidden_size=256, num_layers=1, batch_first=True)
+        self.pooling_layer = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Linear(in_features=256, out_features=128)
 
-    def __init__(self, is_training, reuse=False, name='text_encoder_example'):
-        super(CNNRNNTextEncoder, self).__init__(is_training, reuse=reuse, name=name)
-        self._embedding_size = 128
-        self._margin = 1
 
-        # Define layers for the convolutional network
-        self.conv_layers = nn.ModuleList([nn.Conv2d(in_channels=1, out_channels=128, kernel_size=3, stride=1),
-                                          nn.ReLU(),
-                                          nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1),
-                                          nn.ReLU(),
-                                          nn.BatchNorm2d(128),
-                                          nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1),
-                                          nn.ReLU(),
-                                          nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1),
-                                          nn.ReLU(),
-                                          nn.BatchNorm2d(256)])
+    def forward(self, x):
+        # print(x.shape)
+        x = self.conv_layers(x)
+        # print(x.shape)
+        x, _ = self.gru(x.transpose(1, 2))
+        # print(x.shape)
+        x = self.pooling_layer(x.transpose(1, 2)).squeeze()
+        # print(x.shape, 'here')
+        x = self.fc(x)
+        # print(x.shape)
+        
+        regularizer_loss = 0
+        for param in self.parameters():
+            regularizer_loss += torch.sum(param.pow(2))
+        return x + 0.0005 * regularizer_loss
 
-        # Define the GRU cell for the RNN network
-        self.rnn_cell = nn.GRU(input_size=256, hidden_size=256)
+# To use the model
+model = Text_Encoder()
+input_tensor = torch.randn(32, 64, 768) # assuming a batch size of 32 and input size of (64, 768)
+output_tensor = model(input_tensor.transpose(1, 2)) # Transpose the tensor to be in the (batch_size, input_channels, input_length) format expected by the convolution layers
 
-        # Define the fully connected layers
-        self.fc1 = nn.Linear(in_features=256, out_features=256)
-        self.fc2 = nn.Linear(in_features=256, out_features=128)
-
-    def forward(self, inputs_dict):
-        """Builds the RNN text encoder.
-
-        Returns:
-            encoder_output: The output of the text encoder.
-        """
-
-        caption_batch = inputs_dict['caption_batch']
-        embedding = inputs_dict['embedding_batch']
-        seq_length = compute_sequence_length(caption_batch)
-
-        # Pass the embedding through the convolutional layers
-        net = embedding.unsqueeze(1)  # Add a channel dimension
-        for layer in self.conv_layers:
-            net = layer(net)
-            if isinstance(layer, nn.ReLU):
-                net = F.dropout(net, p=0.5, training=self.is_training)  # Add dropout after ReLU
-            if isinstance(layer, nn.BatchNorm2d):
-                net = F.batch_norm(net, training=self.is_training)
-
-        net = F.max_pool2d(net, kernel_size=(seq_length, 1))  # Apply max pooling over the sequence length dimension
-
-        # Pass the resulting feature map through the RNN network
-        outputs, final_state = self.rnn_cell(net.squeeze(2).permute(1, 0, 2), hx=None)  # Reshape for RNN input
-
-        # Extract the final output of the RNN network
-        net = extract_last_output(outputs.permute(1, 0, 2), seq_length)
-
-        # Pass the final output through the fully connected layers
-        net = self.fc1(net)
-        net = F.relu(net)
-        net = self.fc2(net)
-
-        return {'encoder_output': net}
-
-    @property
-    def embedding_size(self):
-        return self._embedding_size
---
+++ text2shape/models/test_tokenize.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 13 18:26:14 2023
+
+@author: Meysam
+"""
+import numpy as np
+import spacy
+import language_tool_python
+
+
+def pad_texts(texts):
+    max_len = 96
+    padded_texts = []
+    for text in texts:
+        words = text.split()
+        if len(words) < max_len:
+            num_pad = max_len - len(words)
+            words += ['#'] * num_pad
+        padded_texts.append(' '.join(words))
+    return padded_texts
+
+def preprocessing_textlist(text_list):
+    tool = language_tool_python.LanguageTool('en-US') # load the LanguageTool
+    nlp = spacy.load('en_core_web_sm')
+    
+    # create a dictionary of unique tokens
+    # dictionary = {token: idx for idx, token in enumerate(nlp.vocab.strings)}
+    # text_list = pad_texts(text_list)
+    # sequences_idxs = np.empty((len(text_list), 96))
+    decoded_texts = list()
+    
+    for c, text in enumerate(text_list):
+        # lowercase the text
+        text = text.lower()
+        
+        # check for spelling errors and get suggestions
+        matches = tool.check(text)
+        corrections = [match.replacements[0] for match in matches]
+        
+        # replace misspelled words with suggested corrections
+        for i, match in enumerate(matches):
+            start, end = match.offset, match.offset + match.errorLength
+            text = text[:start] + corrections[i] + text[end:]
+        
+        # tokenize and lemmatize the text using Spacy
+        doc = nlp(text)
+        tokens = [token.lemma_.lower() for token in doc]
+        tokens = tokens[:96]
+        decoded_text = text = ' '.join(tokens)
+        decoded_texts.append(decoded_text)
+        
+    return decoded_texts
+
+
+
+text_list = ["The quick brown fox jumped over the lazy dog.", "hello, a good chair!"]
+preprocessed_text_list = preprocessing_textlist(text_list)
+
+
+import torch
+from transformers import BertModel, BertTokenizer
+
+def embed_textlist(text_list):
+    # Load pre-trained BERT model and tokenizer
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertModel.from_pretrained('bert-base-uncased')
+    
+    
+    # Tokenize input texts
+    tokenized_texts = [tokenizer.encode(text, add_special_tokens=True, max_length=64) for text in text_list]
+    
+    # Pad tokenized texts to maximum length
+    max_length = 64
+    padded_texts = [text + [0] * (max_length - len(text)) for text in tokenized_texts]
+    
+    # Convert padded texts to PyTorch tensor
+    input_ids = torch.tensor(padded_texts)
+    
+    # Generate embeddings with lower dimensionality
+    with torch.no_grad():
+        embeddings = model(input_ids)[0][:, :256]
+    
+    print(embeddings.shape)  # Output: torch.Size([2, 128, 256])
+
+
+
+
+
